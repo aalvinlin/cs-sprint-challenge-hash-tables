a2f08fae7db98d41d5fc091faa89968d7e516a1d
Alvin Lin <57117545+aalvinlin@users.noreply.github.com>
2020-06-04 17:14:07 -0700

    begin weights

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -2,6 +2,28 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    needed_weights = dict()
+
+    print("\nlooking in weights", weights, "\n")
+
+    for current_weight_index in range(len(weights)):
+
+        current_weight = weights[current_weight_index]
+
+        # if the key is found, then that means the counterpart to this weight was inserted previously
+        if current_weight in needed_weights:
+
+            # get index of other weight
+            previously_seen_weight_index = weights[current_weight]
+
+            return (current_weight_index, previously_seen_weight_index)
+        
+        else:
+            # store info about this current weight
+            # key: other weight needed to add up to the limit
+            # value: the current weight's index
+            needed_weights[limit - current_weight] = current_weight_index
+
+        print('current needed weights:', needed_weights)
 
     return None
